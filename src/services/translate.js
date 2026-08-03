03b52c0c98ed42ba5a1b5780877e8ff0f6154cc2
bielng <ngunartaban@gmail.com>
2026-08-02 18:39:27 -0600

    implement English–Nuer translation service

--- src/services/translate.js
+++ src/services/translate.js
@@ -1,3 +1,11 @@
 import { Client } from "@gradio/client";
 
 const SPACE_ID = "dayomtechnologies/English_to_Nuer_Translator";
+
+let clientPromise = null;
+function getClient() {
+  if (!clientPromise) {
+    clientPromise = Client.connect(SPACE_ID);
+  }
+  return clientPromise;
+}
